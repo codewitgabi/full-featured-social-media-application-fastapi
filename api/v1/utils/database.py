fa3856c59e3c22387c8af8369685c15bb6145629
Gabriel Michael Ojomakpene <gabrielmicheal497@gmail.com>
2024-08-05 23:40:17 +0100

    fix: fix declarative_base deprecation warning

--- api/v1/utils/database.py
+++ api/v1/utils/database.py
@@ -1,7 +1,6 @@
 import os
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
 load_dotenv()
 
