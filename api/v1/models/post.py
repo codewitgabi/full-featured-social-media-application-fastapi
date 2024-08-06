1b7c799014483422c37afe6c84e4f2ee23d62499
Gabriel Michael Ojomakpene <gabrielmicheal497@gmail.com>
2024-08-06 21:35:28 +0100

    feat: add post comment model

--- api/v1/models/post.py
+++ api/v1/models/post.py
@@ -1,16 +1,16 @@
 from datetime import datetime
-from typing import Optional
+from typing import List, Optional
 from sqlalchemy.orm import mapped_column, Mapped, relationship
 from sqlalchemy import DateTime, ForeignKey, String, func
 from api.v1.models.abstract_base import AbstractBaseModel
-import api
+import api.v1.models as models
 
 
 class Post(AbstractBaseModel):
     __tablename__ = "post"
 
     user_id: Mapped[str] = mapped_column(ForeignKey("user.id"))
-    user: Mapped["api.v1.models.user.User"] = relationship(back_populates="posts")
+    user: Mapped["models.user.User"] = relationship(back_populates="posts")
     content: Mapped[Optional[str]] = mapped_column(String(1024), nullable=True)
     image: Mapped[Optional[str]] = mapped_column(
         String(1024), nullable=True
@@ -18,6 +18,9 @@ class Post(AbstractBaseModel):
     video: Mapped[Optional[str]] = mapped_column(
         String(1024), nullable=True
     )  # video url
+    comments: Mapped[List["models.post_comment.PostComment"]] = relationship(
+        back_populates="post"
+    )
     updated_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True), server_default=func.now(), onupdate=func.now()
     )
