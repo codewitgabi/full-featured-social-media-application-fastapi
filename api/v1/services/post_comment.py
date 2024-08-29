30beb4cee02a503fbab58169ced19727ed80904c
izzyjosh <calebayuba222@gmail.com>
2024-08-30 00:03:02 +0100

    feat: added services to manage comments implementation

---
+++ api/v1/services/post_comment.py
@@ -0,0 +1,104 @@
+from fastapi import HTTPException, status
+from fastapi.encoders import jsonable_encoder
+from sqlalchemy.orm import Session
+from api.v1.schemas.post_comment import CreateCommentSchema, UpdateCommentSchema
+from api.v1.models.post_comment import PostComment
+from api.v1.models.post import Post
+from api.v1.models.user import User
+
+class CommentService:
+    # class attributes
+    post_not_found = HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, 
+            detail="Post does not exist")
+
+    comment_not_found = HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, 
+            detail="Comment does not exist")
+    # end of class attributes
+
+    # class methods
+    def create(self, db: Session, user: User, post_id:str, schema: CreateCommentSchema):
+        schema_dict = schema.model_dump()
+
+        if schema_dict.comment is None:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="The comment cannot be an empty field")
+        
+        # get the post
+        post = db.query(Post).filter(Post.user_id == user.id, Post.id==post_id).first()
+
+        if not post:
+            raise self.post_not_found
+
+        comment = PostComment(post_id = post.id, **schema_dict)
+
+        db.add(comment)
+        db.commit()
+        db.refresh(comment)
+
+        return jsonable_encoder(comment)
+
+
+    def get_comments(self, db: Session, user: User, post_id: str):
+        post = db.query(Post).filter(Post.user_id == user.id, Post.id == post_id).first()
+
+        if not post:
+            raise self.post_not_found
+
+        comments = db.query(PostComment).filter(PostComment.post_id == post_id).all()
+
+        if not comment:
+            raise self.comment_not_found
+
+        return jsonable_encoder(comments)
+
+
+    def delete(self, db:Session, user: User, post_id: str, comment_id: str):
+
+        post = db.query(Post).filter(Post.user_id == user.id, Post.id == post_id).first()
+
+        if not post:
+            raise self.post_not_found
+
+        comment = db.query(PostComment).filter(PostComment.id == comment_id, PostComment.post_id == post.id).first()
+
+        if not comment:
+            raise self.comment_not_found
+
+        db.delete(comment)
+        db.commit()
+
+
+    def update(self, db: Session, user: User, post_id: str, comment_id: str, schema: UpdateCommentSchema):
+
+        schema_dict = schema.model_dump()
+
+        if schema_dict.comment is None:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="The comment cannot be an empty field")
+
+        post = db.query(Post).filter(Post.user_id == user.id, Post.id == post_id).first()
+
+        if not post:
+            raise self.post_not_found
+
+        comment = db.query(PostComment).filter(PostComment.post_id == post.id, PostComment.id == comment_id)
+
+        if not comment:
+            raise self.comment_not_found
+
+        for attr, value in schema_dict.items():
+            if value:
+                setattr(comment, attr, value)
+
+        db.commit()
+        db.refresh(comment)
+
+        return jsonable_encoder(comment)
+
+    # end of class methods
+
+comment_service = CommentService()
