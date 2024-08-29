e711a9d2afce1634b49060dff897030278feefc8
izzyjosh <calebayuba222@gmail.com>
2024-08-30 00:05:15 +0100

    feat: added import for the newly created endpoint

--- api/v1/routes/__init__.py
+++ api/v1/routes/__init__.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 from api.v1.routes.auth import auth
 from api.v1.routes.user import users
 from api.v1.routes.post import posts
-
+from api.v1.routes.post_comment import comments
 
 # version 1 routes
 
@@ -11,3 +11,4 @@ version_one = APIRouter(prefix="/api/v1")
 version_one.include_router(auth)
 version_one.include_router(users)
 version_one.include_router(posts)
+version_one.include_router(comments)
