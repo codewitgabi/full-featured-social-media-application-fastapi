e756b792c0f5888ff8001e560726bdac62bed315
izzyjosh <calebayuba222@gmail.com>
2024-09-06 03:11:58 +0100

    feat: added sharing endpoint

--- api/v1/routes/post.py
+++ api/v1/routes/post.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 
 from api.v1.models.user import User
 from api.v1.responses.success_response import success_response
-from api.v1.schemas.post import CreatePostSchema, UpdatePostSchema
+from api.v1.schemas.post import CreatePostSchema, UpdatePostSchema, RepostCreate
 from api.v1.utils.dependencies import get_db
 from api.v1.services.user import user_service
 from api.v1.services.post import post_service
@@ -59,29 +59,50 @@ async def update_post(
         data=updated_post,
     )
 
-@posts.patch("/{id}/like",status_code=status.HTTP_200_OK)
-async def like_post(
-        id: str,
-        db: Session = Depends(get_db),
-        user: User = Depends(user_service.get_current_user)):
 
-    liked_post = post_service.like_post(db=db,user=user,post_id=id)
+@posts.patch("/{id}/like", status_code=status.HTTP_200_OK)
+async def like_post(
+    id: str,
+    db: Session = Depends(get_db),
+    user: User = Depends(user_service.get_current_user),
+):
+
+    liked_post = post_service.like_post(db=db, user=user, post_id=id)
 
     return success_response(
-            status_code=status.HTTP_200_OK,
-            message="Post updated successfully",
-            )
+        status_code=status.HTTP_200_OK,
+        message="Post updated successfully",
+    )
 
-@posts.get("/{id}/like", status_code = status.HTTP_200_OK)
+
+@posts.get("/{id}/like", status_code=status.HTTP_200_OK)
 async def get_likes(
-        id: str,
-        db: Session = Depends(get_db),
-        user: User = Depends(user_service.get_current_user)):
+    id: str,
+    db: Session = Depends(get_db),
+    user: User = Depends(user_service.get_current_user),
+):
 
     likes = post_service.get_likes(db=db, post_id=id, user=user)
 
     return success_response(
-            status_code=status.HTTP_200_OK,
-            message="Likes returned successfully",
-            data = likes,)
+        status_code=status.HTTP_200_OK,
+        message="Likes returned successfully",
+        data=likes,
+    )
 
+
+@posts.post("/{id}/repost", status_code=status.HTTP_201_CREATED)
+async def repost(
+    id: str,
+    schema: RepostCreate,
+    db: Session = Depends(get_db),
+    user: User = Depends(user_service.get_current_user),
+):
+
+    repost = post_service.repost(db=db, post_id=id, schema=schema, user=user)
+
+    return success_response(
+        status_code=status.HTTP_201_CREATED,
+        message="Reposted successfully",
+        data=repost,
+    )
--- api/v1/schemas/post.py
+++ api/v1/schemas/post.py
@@ -1,5 +1,7 @@
-from pydantic import BaseModel, ConfigDict, UUID4
+from pydantic import BaseModel, ConfigDict, UUID4, Field
 from api.v1.schemas.user import UserResponse
+from datetime import datetime
+
 
 class CreatePostSchema(BaseModel):
     model_config = ConfigDict(from_attributes=True)
@@ -17,6 +19,15 @@ class UpdatePostSchema(BaseModel):
     video: str | None = None
 
 
+class PostResponse(CreatePostSchema):
+
+    id: UUID4
+    user_id: UUID4 = Field(exclude=True)
+    created_at: datetime
+    updated_at: datetime
+    user: UserResponse
+
+
 class LikeResponse(BaseModel):
     model_config = ConfigDict(from_attribute=True)
 
@@ -25,3 +36,21 @@ class LikeResponse(BaseModel):
     user_id: UUID4
     liked: bool
     user: UserResponse = None
+
+
+class RepostCreate(BaseModel):
+
+    content: str | None = None
+
+
+class RepostResponse(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: str
+    post_id: UUID4 = Field(exclude=True)
+    user_id: UUID4 = Field(exclude=True)
+    content: str | None = None
+    created_at: datetime
+    updated_at: datetime
+    user: UserResponse
+    post: PostResponse
